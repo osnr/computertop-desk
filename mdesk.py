e7decee0e56fc8b9d2916f8547eb38ce8ec4db8e
Omar Rizwan <omar.rizwan@gmail.com>
2018-02-17 15:29:34 -0800

    Better coords.

--- mdesk.py
+++ mdesk.py
@@ -11,7 +11,7 @@ import cv2
 
 if __name__ == '__main__':
     width = 1280
-    height = 720
+    height = 800 - 22 # - title bar height
 
     # create image
     image = np.ones((height, width, 3), dtype=np.float32)
@@ -22,7 +22,13 @@ if __name__ == '__main__':
 
     window_name = 'projector'
     cv2.namedWindow(window_name)
-    cv2.moveWindow(window_name, 1500, -50)
+    cv2.moveWindow(window_name, 1680, 0)
     cv2.imshow(window_name, image)
-    cv2.waitKey()
+
+    cam = cv2.VideoCapture(1)
+    while True:
+        ret_val, img = cam.read()
+        cv2.imshow('my webcam', img)
+        if cv2.waitKey(1) == 27: break
+    
     cv2.destroyAllWindows()
