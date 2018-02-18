e2201f431e2fdad12b0899d0600b69eae5717b0f
Omar Rizwan <omar.rizwan@gmail.com>
2018-02-17 19:15:45 -0800

    Rect detection.

--- mdesk.py
+++ mdesk.py
@@ -8,7 +8,74 @@ Created on Thu Jun 22 16:44:27 2017
 
 import numpy as np
 import time
+import pickle
 import cv2
+
+def calibrate():
+    print("Press key to begin calibration.")
+    cv2.waitKey()
+
+    camera_pts = []
+    def calibration_click(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            camera_pts.append([x * (camera_width / width), y * (camera_height / height)])
+            print(camera_pts)
+
+
+    ret_val, img = cam.read()
+    cv2.imshow('camera calibrator', cv2.resize(img, (width, height)))
+    cv2.setMouseCallback("camera calibrator", calibration_click)
+
+    while True:
+        # ret_val, img = cam.read()
+        # cv2.imshow('camera calibrator', cv2.resize(img, (width, height)))
+        if cv2.waitKey(1) == 27: break
+
+    return camera_pts
+
+def get_new(old):
+    new = np.zeros(old.shape, np.uint8)
+    return new
+
+def paperdet(orig):
+    # these constants are carefully picked
+    MORPH = 9
+    CANNY = 84
+    HOUGH = 25
+
+    img = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
+    cv2.GaussianBlur(img, (3,3), 0, img)
+
+    # this is to recognize white on white
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(MORPH,MORPH))
+    dilated = cv2.dilate(img, kernel)
+
+    edges = cv2.Canny(dilated, 0, CANNY, apertureSize=3)
+
+    # lines = cv2.HoughLinesP(edges, 1,  3.14/180, HOUGH)
+    # for line in lines[0]:
+    #      cv2.line(edges, (line[0], line[1]), (line[2], line[3]),
+    #                      (255,0,0), 2, 8)
+
+    # finding contours
+    _, contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS)
+    contours = filter(lambda cont: cv2.arcLength(cont, False) > 100, contours)
+    contours = filter(lambda cont: cv2.contourArea(cont) > 10000, contours)
+
+    # simplify contours down to polygons
+    rects = []
+    for cont in contours:
+        rect = cv2.approxPolyDP(cont, 40, True).copy().reshape(-1, 2)
+        rects.append(rect)
+
+    # # that's basically it
+    # cv2.drawContours(orig, rects,-1,(0,255,0),1)
+
+    # show only contours
+    new = get_new(img)
+    cv2.drawContours(new, rects,-1,(255,255,0),1)
+    return new
+
 
 if __name__ == '__main__':
     width = 1280
@@ -36,37 +103,28 @@ if __name__ == '__main__':
     cv2.moveWindow(window_name, 1680, 0)
     cv2.imshow(window_name, image)
 
-    print("Press key to begin calibration.")
-    cv2.waitKey()
-
     cam = cv2.VideoCapture(1)
 
     camera_width = 1920
     camera_height = 1080
-    camera_pts = []
-    def calibration_click(event, x, y, flags, param):
-        if event == cv2.EVENT_LBUTTONDOWN:
-            camera_pts.append([x * (camera_width / width), y * (camera_height / height)])
-            print(camera_pts)
-
-
-    ret_val, img = cam.read()
-    cv2.imshow('camera calibrator', cv2.resize(img, (width, height)))
-    cv2.setMouseCallback("camera calibrator", calibration_click)
-
-    while True:
-        # ret_val, img = cam.read()
-        # cv2.imshow('camera calibrator', cv2.resize(img, (width, height)))
-        if cv2.waitKey(1) == 27: break
+    try:
+        camera_pts = pickle.load(open("camera_pts.p", "rb"))
+    except:
+        camera_pts = calibrate()
+        pickle.dump(camera_pts, open("camera_pts.p", "wb"))
 
     M, status = cv2.findHomography(np.float32(camera_pts), np.float32(project_pts))
-    print(M)
 
     while True:
         ret_val, img = cam.read()
+
         rows, cols, ch = img.shape
-        warped = cv2.warpPerspective(img, M, (cols, rows))
-        cv2.imshow('projector', warped[0:height, 0:width])
+        warped = cv2.warpPerspective(img, M, (cols, rows))[0:height, 0:width]
+        # cv2.imshow('projector', warped)
+
+        det = paperdet(warped)
+        cv2.imshow('projector', det)
+
         if cv2.waitKey(1) == 27: break
 
     cv2.destroyAllWindows()
